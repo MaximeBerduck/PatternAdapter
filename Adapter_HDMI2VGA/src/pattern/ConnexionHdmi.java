bd50cd121dd702f2e31cceb76ca44e674cfacac7
maxime <maxime.berduck@etu.unilim.fr>
2020-04-17 13:04:15 +0200

    Revert "html"
    This reverts commit 3d676c3e5fa00e9760394f0da2f9465d29c94760, reversing
    changes made to 44273f462a4dc2f3f003c12a93858769de641a69.

--- Adapter_HDMI2VGA/src/pattern/ConnexionHdmi.java
+++
@@ -1,5 +0,0 @@
-package pattern;
-
-public interface ConnexionHdmi {
-	public abstract void seConnecterEnHdmi();
-}
--- Adapter_HDMI2VGA/src/pattern/ConnexionVga.java
+++
@@ -1,5 +0,0 @@
-package pattern;
-
-public interface ConnexionVga {
-	public abstract void seConnecterEnVga();
-}
